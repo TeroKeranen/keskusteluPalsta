85a49dc81a35b7b6bb172a7f724ec80b0880ed2d
Tero Keränen <tero-keranen@hotmail.com>
2023-02-13 13:04:16 +0200

    testing register page

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body.username);
+  console.log(req.body.password);
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.listen(3000, function () {
   console.log("server start on port 3000");
 });
