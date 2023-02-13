552f6ad652f724526d9e660dd4f31a6f70485921
Tero Keränen <tero-keranen@hotmail.com>
2023-02-13 20:16:28 +0200

    testing the registration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
@@ -10,6 +11,14 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Mongoose db
+const dbUser = process.env.MONGODB_URI;
+//Mongoose connect
+mongoose
+  .connect(dbUser, { useNewUrlParser: true })
+  .then((result) => console.log("connected to database"))
+  .catch((err) => console.log(err));
+
 app.use(
   session({
     secret: "fairytale is safe",
@@ -17,20 +26,12 @@ app.use(
     saveUninitialized: false,
   })
 );
-
+app.use(express.json());
 // passport initialize middleware
 app.use(passport.initialize());
 //
 // passport session middleware
 app.use(passport.session());
-
-// Mongoose db
-const dbUser = process.env.MONGO_URI;
-//Mongoose connect
-mongoose
-  .connect(dbUser, { useNewUrlParser: true })
-  .then((result) => console.log("connected to database"))
-  .catch((err) => console.log(err));
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
@@ -44,9 +45,31 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body.username);
-  console.log(req.body.password);
+app.post("/register", async (req, res) => {
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  // Check if username is taken
+  const nameExist = await User.exists({ username: userName });
+
+  if (!nameExist) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) return nameExist(err);
+
+      bcrypt.hash(password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        const newUser = new User({
+          username: userName,
+          password: hash,
+        });
+
+        newUser.save();
+
+        res.redirect("/login");
+      });
+    });
+  }
 });
 
 app.get("/login", (req, res) => {
