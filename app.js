755e583cc76ef1f84e2f855fa8eb01a3a6c23242
Tero Keränen <tero-keranen@hotmail.com>
2023-02-13 19:54:58 +0200

    enable PassportJS and mongodb setting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
+const User = require("./models/user");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -11,7 +12,7 @@ app.use(express.static("public"));
 
 app.use(
   session({
-    secret: process.env.sessionSecret,
+    secret: "fairytale is safe",
     resave: false,
     saveUninitialized: false,
   })
@@ -23,7 +24,17 @@ app.use(passport.initialize());
 // passport session middleware
 app.use(passport.session());
 
+// Mongoose db
+const dbUser = process.env.MONGO_URI;
 //Mongoose connect
+mongoose
+  .connect(dbUser, { useNewUrlParser: true })
+  .then((result) => console.log("connected to database"))
+  .catch((err) => console.log(err));
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("index");
